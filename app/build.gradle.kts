4fd1c408ac2a8ac728f11aa927b977ec777cc792
Louis Parton <louisparton0@gmail.com>
2023-08-27 18:10:34 -0400

    Assignment Week 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 }
 
 android {
+
     namespace = "com.louisparton.timefighter"
     compileSdk = 33
 
@@ -32,6 +33,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        buildConfig = true // <- set this to true
     }
 }
 
